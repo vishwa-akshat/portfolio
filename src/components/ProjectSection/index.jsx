02f9180a5c69bec2c5ba0720c5665a7a7f97a2c3
vishwa-akshat <akvs0707@gmail.com>
2023-04-25 12:22:53 +0530

    feat: resume update and project added

--- src/components/ProjectSection/index.jsx
+++ src/components/ProjectSection/index.jsx
@@ -1,76 +1,85 @@
 import RockPaperAndScissorImg from "images/RockPaperAndScissor.png";
 import PortfolioImg from "images/portfolio.png";
+import WindbnbImg from "images/Windbnb.png";
 
 import "./style.scss";
 
 function ProjectSection() {
-  const projects = [
-    {
-      id: "01",
-      name: "Rock Paper and Scissor",
-      img: RockPaperAndScissorImg,
-      stackArr: ["React", "sass"],
-      codeUrl: "https://github.com/vishwa-akshat/rock-paper-scissors",
-      projectUrl:
-        "https://rock-paper-scissors-i2z0nwylr-vishwa-akshat.vercel.app/",
-    },
-    {
-      id: "02",
-      name: "Portfolio",
-      img: PortfolioImg,
-      stackArr: ["React", "sass"],
-      codeUrl: "https://github.com/vishwa-akshat/portfolio",
-      projectUrl: "https://akshat-dev.vercel.app/",
-    },
-  ];
+    const projects = [
+        {
+            id: "01",
+            name: "Rock Paper and Scissor",
+            img: RockPaperAndScissorImg,
+            stackArr: ["React", "sass"],
+            codeUrl: "https://github.com/vishwa-akshat/rock-paper-scissors",
+            projectUrl:
+                "https://rock-paper-scissors-i2z0nwylr-vishwa-akshat.vercel.app/",
+        },
+        {
+            id: "02",
+            name: "Portfolio",
+            img: PortfolioImg,
+            stackArr: ["React", "SASS"],
+            codeUrl: "https://github.com/vishwa-akshat/portfolio",
+            projectUrl: "https://akshat-dev.vercel.app/",
+        },
+        {
+            id: "03",
+            name: "Windbnb",
+            img: WindbnbImg,
+            stackArr: ["Vite", "React", "SASS", "Zustand"],
+            codeUrl: "https://github.com/vishwa-akshat/windbnb",
+            projectUrl: "https://windbnb-7xzt4n3ln-vishwa-akshat.vercel.app/",
+        },
+    ];
 
-  return (
-    <div className="projects-section">
-      <h1 className="heading">Projects</h1>
-      <div className="projects-wrapper">
-        {projects.map((project, index) => {
-          return (
-            <div key={index} className="wrapper">
-              <div className="project-number">{project.id}</div>
-              <div className="project-img-wrapper">
-                <img
-                  src={project.img}
-                  className="project-img"
-                  alt={project.name}
-                />
-                <div className="backdrop">
-                  <a
-                    className="links"
-                    href={project.projectUrl}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    View Project
-                  </a>
-                  <a
-                    className="links"
-                    href={project.codeUrl}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    View Code
-                  </a>
-                </div>
-              </div>
-              <h3 className="name">{project.name}</h3>
-              <p className="stack-wrapper">
-                {project.stackArr.map((stack, index) => (
-                  <span key={index} className="stack">
-                    {stack}
-                  </span>
-                ))}
-              </p>
+    return (
+        <div className="projects-section">
+            <h1 className="heading">Projects</h1>
+            <div className="projects-wrapper">
+                {projects.map((project, index) => {
+                    return (
+                        <div key={index} className="wrapper">
+                            <div className="project-number">{project.id}</div>
+                            <div className="project-img-wrapper">
+                                <img
+                                    src={project.img}
+                                    className="project-img"
+                                    alt={project.name}
+                                />
+                                <div className="backdrop">
+                                    <a
+                                        className="links"
+                                        href={project.projectUrl}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        View Project
+                                    </a>
+                                    <a
+                                        className="links"
+                                        href={project.codeUrl}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        View Code
+                                    </a>
+                                </div>
+                            </div>
+                            <h3 className="name">{project.name}</h3>
+                            <p className="stack-wrapper">
+                                {project.stackArr.map((stack, index) => (
+                                    <span key={index} className="stack">
+                                        {stack}
+                                    </span>
+                                ))}
+                            </p>
+                        </div>
+                    );
+                })}
             </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default ProjectSection;
