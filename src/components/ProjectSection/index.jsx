73b27a4492db1cede99249dec15b634734d3ae19
vishwa-akshat <akvs0707@gmail.com>
2024-04-23 19:46:19 +0530

    updated projects

--- src/components/ProjectSection/index.jsx
+++ src/components/ProjectSection/index.jsx
@@ -1,9 +1,7 @@
-import RockPaperAndScissorImg from "images/RockPaperAndScissor.png";
 import PortfolioImg from "images/portfolio.png";
-import WindbnbImg from "images/Windbnb.png";
-import WeatherAppImg from "images/WeatherApp.png";
-import ImageUploaderImg from "images/ImageUploader.png";
-import MetaversusImg from "images/Metaversus.png";
+import HirerhinosImg from "images/hirerhinos.png";
+import RivallyticsImg from "images/rivallytics.png";
+
 import ThulloImg from "images/Thullo.png";
 import PinnacleImg from "images/pinnacle.png";
 
@@ -13,14 +11,34 @@ function ProjectSection() {
     const projects = [
         {
             id: "01",
-            name: "Pinnacle Doodle - (WIP)",
+            name: "Pinnacle Doodle",
             img: PinnacleImg,
             stackArr: ["Vite", "React", "Styled-Component", "Konva.js"],
-            codeUrl: "https://github.com/vishwa-akshat/pinnacle-doodle.git",
-            projectUrl: "https://github.com/vishwa-akshat/pinnacle-doodle.git",
         },
         {
-            id: "01",
+            id: "02",
+            name: "Hirerhinos",
+            img: HirerhinosImg,
+            stackArr: [
+                "Next.js",
+                "Zustand",
+                "swr",
+                "Node.js",
+                "Express.js",
+                "Mongodb",
+                "Websockets",
+                "Chakra UI",
+                "React Lottie",
+            ],
+        },
+        {
+            id: "03",
+            name: "Rivallytics",
+            img: RivallyticsImg,
+            stackArr: ["Next.js", "Redux", "Chakra UI", "Nivo.js"],
+        },
+        {
+            id: "04",
             name: "Thullo",
             img: ThulloImg,
             stackArr: [
@@ -32,64 +50,6 @@ function ProjectSection() {
                 "Firebase",
                 "DnD Kit",
             ],
-            codeUrl: "https://github.com/vishwa-akshat/thullo",
-            projectUrl: "https://thullo-three.vercel.app/",
-        },
-        {
-            id: "02",
-            name: "Metaversus",
-            img: MetaversusImg,
-            stackArr: ["Next.js", "React", "Tailwind CSS", "Framer Motion"],
-            codeUrl: "https://github.com/vishwa-akshat/metaversus",
-            projectUrl: "https://metaversus-lake.vercel.app/",
-        },
-        {
-            id: "03",
-            name: "Weather App",
-            img: WeatherAppImg,
-            stackArr: ["Vite", "React", "SASS", "Zustand", "Openweather API"],
-            codeUrl: "https://github.com/vishwa-akshat/weather-app",
-            projectUrl: "https://weather-app-red-one.vercel.app/",
-        },
-        {
-            id: "04",
-            name: "Rock Paper and Scissor",
-            img: RockPaperAndScissorImg,
-            stackArr: ["React", "sass"],
-            codeUrl: "https://github.com/vishwa-akshat/rock-paper-scissors",
-            projectUrl:
-                "https://rock-paper-scissors-i2z0nwylr-vishwa-akshat.vercel.app/",
-        },
-        {
-            id: "05",
-            name: "Image Uploader",
-            img: ImageUploaderImg,
-            stackArr: [
-                "Vite",
-                "React",
-                "Typescript",
-                "SASS",
-                "Zustand",
-                "Firebase",
-            ],
-            codeUrl: "https://github.com/vishwa-akshat/image-uploader",
-            projectUrl: "https://image-uploader-smoky.vercel.app/",
-        },
-        {
-            id: "06",
-            name: "Portfolio",
-            img: PortfolioImg,
-            stackArr: ["React", "SASS", "Hashnode API"],
-            codeUrl: "https://github.com/vishwa-akshat/portfolio",
-            projectUrl: "https://akshat-dev.vercel.app/",
-        },
-        {
-            id: "07",
-            name: "Windbnb",
-            img: WindbnbImg,
-            stackArr: ["Vite", "React", "SASS", "Zustand"],
-            codeUrl: "https://github.com/vishwa-akshat/windbnb",
-            projectUrl: "https://windbnb-7xzt4n3ln-vishwa-akshat.vercel.app/",
         },
     ];
 
@@ -107,24 +67,6 @@ function ProjectSection() {
                                     className="project-img"
                                     alt={project.name}
                                 />
-                                <div className="backdrop">
-                                    <a
-                                        className="links"
-                                        href={project.projectUrl}
-                                        target="_blank"
-                                        rel="noreferrer"
-                                    >
-                                        View Project
-                                    </a>
-                                    <a
-                                        className="links"
-                                        href={project.codeUrl}
-                                        target="_blank"
-                                        rel="noreferrer"
-                                    >
-                                        View Code
-                                    </a>
-                                </div>
                             </div>
                             <h3 className="name">{project.name}</h3>
                             <p className="stack-wrapper">
